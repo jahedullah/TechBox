cb5cc2592eda04e56a5c9d05114d11519cb408e9
jahedullah <jahedulislam6060@gmail.com>
2023-01-19 15:14:03 +0600

    AccessToken Working state

--- src/main/java/io/welldev/techbox/authentication/dto/AccessTokenDto.java
+++ src/main/java/io/welldev/techbox/authentication/dto/AccessTokenDto.java
@@ -1,8 +1,10 @@
 package io.welldev.techbox.authentication.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class AccessTokenDto {
     private String newAccessToken;
 }
